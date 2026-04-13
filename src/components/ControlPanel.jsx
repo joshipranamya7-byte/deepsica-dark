3402a44ac0f15838bcc0fa7af1fa3d76e44853cd
Pranamya <joshipranamya7@gmail.com>
2026-04-14 00:35:09 +0530

    changed default text to aman

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -13,7 +13,7 @@ const SHAPES = [
 const PRESETS = ['GOTHAM', 'SHADOWS', 'DARKKNIGHT', 'VIGILANTE', 'ARKHAM']
 
 export default function ControlPanel({ onMorph, activeShape }) {
-  const [inputText, setInputText] = useState('DEEPSICA')
+  const [inputText, setInputText] = useState('AMAN BHOSADIWALA')
   const [particleCount, setParticleCount] = useState(3500)
 
   const handleTextMorph = () => {
